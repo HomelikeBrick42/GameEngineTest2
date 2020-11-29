e1f05b1276baa26fffdb095e9ffde77f402f2c2c
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2020-11-29 15:49:48 +1300

    Added Project

---
+++ README.md
@@ -0,0 +1,16 @@
+# BrickEngine
+This is a early stage game engine.
+## Getting Started
+Currently tested compilers
+  - Visual Studio 2019
+
+Clone the repository with `git clone --recursive https://github.com/HomelikeBrick42/GameEngineTest2`.
+
+## Features
+  - Comming Soon
+
+## What's Next?
+  - Entity Component System
+  - 3D Renderer
+  - 2D Renderer
+  - Textures
---
+++ premake5.lua
@@ -0,0 +1,168 @@
+workspace "BrickEngine"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+	
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["spdlog"] = "%{wks.location}/BrickEngine/vendor/spdlog/include"
+IncludeDir["GLFW"] = "%{wks.location}/BrickEngine/vendor/GLFW/include"
+IncludeDir["glad"] = "%{wks.location}/BrickEngine/vendor/glad/include"
+IncludeDir["ImGui"] = "%{wks.location}/BrickEngine/vendor/imgui"
+IncludeDir["glm"] = "%{wks.location}/BrickEngine/vendor/glm"
+IncludeDir["entt"] = "%{wks.location}/BrickEngine/vendor/entt/single_include"
+IncludeDir["stb_image"] = "%{wks.location}/BrickEngine/vendor/stb_image"
+
+group "Dependencies"
+	include "BrickEngine/vendor/GLFW"
+	include "BrickEngine/vendor/glad"
+	include "BrickEngine/vendor/imgui"
+group ""
+	
+project "BrickEngine"
+	location "BrickEngine"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "brickpch.hpp"
+	pchsource "%{prj.name}/src/brickpch.cpp"
+	
+	files
+	{
+		"%{prj.name}/src/**.hpp",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl",
+		"%{prj.name}/vendor/stb_image/stb_image.cpp"
+	}
+	
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.entt}",
+		"%{IncludeDir.stb_image}"
+	}
+	
+	links
+	{
+		"GLFW",
+		"glad",
+		"ImGui",
+		"opengl32.lib"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLM_FORCE_SWIZZLE",
+		"GLM_FORCE_LEFT_HANDED"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"BRICKENGINE_PLATFORM_WINDOWS",
+			"GLFW_INCLUDE_NONE"
+		}
+
+	filter "configurations:Debug"
+		defines "BRICKENGINE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "BRICKENGINE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "BRICKENGINE_DIST"
+		runtime "Release"
+		optimize "on"
+		
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+	
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	pchheader "pch.hpp"
+	pchsource "%{prj.name}/src/pch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.hpp",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{wks.location}/BrickEngine/src",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.entt}"
+	}
+
+	links
+	{
+		"BrickEngine"
+	}
+
+	defines
+	{
+		"GLM_FORCE_SWIZZLE",
+		"GLM_FORCE_LEFT_HANDED"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"BRICKENGINE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "BRICKENGINE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "BRICKENGINE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "BRICKENGINE_DIST"
+		runtime "Release"
+		optimize "on"
